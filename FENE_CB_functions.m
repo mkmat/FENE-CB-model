917b708d2c616c97607de0478e6b028d75657175
Martin Kröger <mk@mat.ethz.ch>
2024-12-09 11:40:39 +0100

    Update FENE_CB_functions.m

--- FENE_CB_functions.m
+++ FENE_CB_functions.m
@@ -120,13 +120,6 @@ for kappa = [10 20 30 50 75 100]
   semilogx(t,Rg_logarithmic(kappa,t),'k.-'); hold on; 
 end
 
-% Example: Rg (logarithmic fit) versus time for different kappa
-figure;
-t = 10.^linspace(0,5,200); 
-for kappa = [10 20 30 50 75 100]
-  plot(t,Rg_logarithmic(kappa,t),'k.-'); hold on; 
-end
-
 % Example Rg (power-law fit) versus kappa at three different times
 figure;
 kappas = linspace(10,100,100);
